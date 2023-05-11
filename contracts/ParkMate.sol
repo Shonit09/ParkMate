5ba34ca886817626e9ad529c69113649ea2c38cc
Shonit <shonit.saahil@gmail.com>
2023-05-11 23:54:52 +0530

    minor tweak

--- contracts/ParkMate.sol
+++ contracts/ParkMate.sol
@@ -1,22 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract ParkMate{
-    uint public count;
-    function get() public view returns(uint){
-        return count;
-    }
-
-    function inc() public{
-        count+=1;
-    }
-
-    function dec() public{
-        count-=1;
-    }
-}
-
-/*
 contract ParkMate {
 
     address public owner;
@@ -52,4 +36,3 @@ contract ParkMate {
         return balance;
     }
 }
-*/
